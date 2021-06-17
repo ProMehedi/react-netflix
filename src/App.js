612a9c3dd74d143461eb1f0bfe5adffff99492ea
promehedi <mmhasan563@gmail.com>
2021-06-17 21:08:50 +0600

    Setup React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const App = () => {
-  return <div className='text-red-700 text-5xl font-bold text-center'>Hello Netflix</div>
+  return (
+    <>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
+  )
 }
 
 export default App
