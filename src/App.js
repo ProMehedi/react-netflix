eb75457b80222f31a5d588852e1aaac376498d9d
promehedi <mmhasan563@gmail.com>
2021-06-17 20:58:11 +0600

    Setup Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return <div>Hello Netflix</div>
+  return <div className='text-red-700 text-5xl font-bold text-center'>Hello Netflix</div>
 }
 
 export default App
