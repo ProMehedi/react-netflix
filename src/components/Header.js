18abeed4886bb42af72eef02f3fb72ee637a3d88
promehedi <mmhasan563@gmail.com>
2021-06-19 16:57:34 +0600

    Fix Responsive Issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import GetStartedForm from './GetStartedForm'
 import Navbar from './Navbar'
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <div className='px-5 md:px-14 mx-auto flex justify-between items-center'>
       <Link to='/'>
-        <img src='/images/logo.svg' alt='Netflix' width='170' />
+        <img src='/images/logo.svg' alt='Netflix' width='108' />
       </Link>
       <div>
         <Link
           to='/login'
-          className='inline-block bg-red-600 text-white font-medium tex-base px-5 py-2 rounded'
+          className='inline-block bg-red-600 text-white font-medium tex-base px-4 py-1 rounded'
         >
           Sign In
         </Link>
