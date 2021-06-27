c6bfd86644fb07254982ce763124259bbf70349c
promehedi <mmhasan563@gmail.com>
2021-06-27 13:33:19 +0600

    FirebaseContext

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { firebase } from './config/firebase.prod'
+import FirebaseContext from './context/FirebaseContext'
 
 ReactDOM.render(
-  <>
+  <FirebaseContext.Provider value={{ firebase }}>
     <Router>
       <App />
     </Router>
-  </>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 )
