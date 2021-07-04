c189421d66a0d44ca78cb4ac56cd8c8457561848
promehedi <mmhasan563@gmail.com>
2021-07-04 16:01:47 +0600

    Firebase Signup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Signin from './pages/Signin'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/signin' component={Signin} />
+        <Route exact path='/signup' component={Signup} />
         <Route component={NotFound} />
       </Switch>
       <Footer />
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 import HeaderContainer from '../components/header/HeaderContainer'
 import FirebaseContext from '../context/FirebaseContext'
 
-const Signin = ({ history }) => {
+const Signup = ({ history }) => {
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [message, setMessage] = useState('')
-  const isValid = email === '' || password === ''
+  const isValid = name === '' || email === '' || password === ''
 
   const { firebase } = useContext(FirebaseContext)
 
@@ -17,14 +18,18 @@ const Signin = ({ history }) => {
     try {
       const reqAuth = await firebase
         .auth()
-        .signInWithEmailAndPassword(email, password)
+        .createUserWithEmailAndPassword(email, password)
 
-      console.log(reqAuth)
+      await reqAuth.user.updateProfile({
+        displayName: name,
+        photoURL: `/images/users/${Math.floor(Math.random() * 5) + 1}.png`,
+      })
 
       // Redirect to home page
-      history.push('/')
+      history.push('/signin')
 
       // Clear Form Data
+      setName('')
       setEmail('')
       setPassword('')
       setMessage('')
@@ -39,9 +44,17 @@ const Signin = ({ history }) => {
         <div className='container mx-auto px-4 flex justify-center'>
           <div className='my-5 py-5 md:py-8 md:pb-40 px-8 md:px-10 lg:px-20 text-white w-full md:w-8/12 lg:w-6/12 bg-black bg-opacity-75 rounded'>
             <h4 className='font-bold text-base md:text-2xl xl:text-3xl mb-3 md:mb-5'>
-              Sign In
+              Sign Up
             </h4>
             <form onSubmit={submitHandler}>
+              <input
+                className='w-full px-4 py-3 bg-gray-800 rounded mb-4'
+                type='text'
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder='Enter full name'
+              />
               <input
                 className='w-full px-4 py-3 bg-gray-800 rounded mb-4'
                 type='email'
@@ -63,18 +76,14 @@ const Signin = ({ history }) => {
                 disabled={isValid}
                 className='w-full font-semibold text-center text-white duration-200 bg-red-600 focus:bg-red-700 hover:bg-red-700 px-4 py-3 text-base rounded disabled:opacity-50 disabled:cursor-not-allowed'
               >
-                Sign In
+                Sign Up
               </button>
               <p className='text-gray-400 my-3'>
-                New to Netflix?
-                <Link to='/signup' className='text-white font-bold ml-1'>
-                  Sign Up Now
+                Already have an account??
+                <Link to='/signin' className='text-white font-bold ml-1'>
+                  Sign In Now
                 </Link>
                 .
-              </p>
-              <p className='text-gray-400 text-sm mb-4'>
-                This page is protected by Google reCAPTCHA to ensure you're not
-                a bot.
               </p>
               {message && (
                 <p className='text-red-900 font-semibold text-xs bg-red-300 px-4 py-2 rounded'>
@@ -89,4 +98,4 @@ const Signin = ({ history }) => {
   )
 }
 
-export default Signin
+export default Signup
